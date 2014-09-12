4806891ccc027b0ee251ee9d36de38fb099e71a0
Dani Dirks <dani.dirks@build.com>
2014-09-12 15:39:26 -0500

    Week9 code js git push

--- scripts/danisaurus.js
+++ scripts/danisaurus.js
@@ -14,14 +14,14 @@ $(document).ready(function(){
 	});
 
 
-	$("pre").snippet("html");
+	$("pre").each.snippet("html");
 	    // Finds <pre> elements with the class "htmlCode"
 	    // and snippet highlights the HTML code within.
-	$("pre").snippet("css",{style:"sh_whitengrey"});
+	$("pre").each.snippet("css",{style:"sh_whitengrey"});
 	    // Finds <pre> elements with the class "styles"
 	    // and snippet highlights the CSS code within
 	    // using the "greenlcd" styling.
-	$("pre").snippet("javascript",{style:"random",transparent:true,showNum:false});
+	$("pre").each.snippet("javascript",{style:"random",transparent:true,showNum:false});
 	    // Finds <pre> elements with the class "js"
 	    // and snippet highlights the JAVASCRIPT code within
 	    // using a random style from the selection of 39
