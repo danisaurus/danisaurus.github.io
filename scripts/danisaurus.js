60418426220a0f47500c5574f87f55bb437cfb63
Dani Dirks <dani.dirks@build.com>
2014-09-12 15:33:04 -0500

    WEEK 9 Editing code snippet stylings

--- scripts/danisaurus.js
+++ scripts/danisaurus.js
@@ -12,4 +12,20 @@ $(document).ready(function(){
 			$dropdownMenu.removeClass('show').addClass('hide');
 		}
 	});
+
+
+	$("pre.code").snippet("html");
+	    // Finds <pre> elements with the class "htmlCode"
+	    // and snippet highlights the HTML code within.
+	$("pre.codeStyle").snippet("css",{style:"sh_whitengrey"});
+	    // Finds <pre> elements with the class "styles"
+	    // and snippet highlights the CSS code within
+	    // using the "greenlcd" styling.
+	$("pre").snippet("javascript",{style:"random",transparent:true,showNum:false});
+	    // Finds <pre> elements with the class "js"
+	    // and snippet highlights the JAVASCRIPT code within
+	    // using a random style from the selection of 39
+	    // with a transparent background
+	    // without showing line numbers.
 });
+
