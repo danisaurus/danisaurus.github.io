3fc3ab44700fd33c2514b9f1deafdb1e1ba500d0
Dani Dirks <dani.dirks@build.com>
2014-09-12 15:35:29 -0500

    Week9 code snippets

--- scripts/danisaurus.js
+++ scripts/danisaurus.js
@@ -14,10 +14,10 @@ $(document).ready(function(){
 	});
 
 
-	$("pre.code").snippet("html");
+	$("pre").snippet("html");
 	    // Finds <pre> elements with the class "htmlCode"
 	    // and snippet highlights the HTML code within.
-	$("pre.codeStyle").snippet("css",{style:"sh_whitengrey"});
+	$("pre").snippet("css",{style:"whitengrey"});
 	    // Finds <pre> elements with the class "styles"
 	    // and snippet highlights the CSS code within
 	    // using the "greenlcd" styling.
